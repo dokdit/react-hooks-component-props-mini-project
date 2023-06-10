cf769ee292815a7ef2557793fa474ad67af027c6
AK <akueiminyang@cmail.carleton.ca>
2023-06-10 11:10:11 -0400

    Lab completed

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,25 @@
 import React from "react";
 
-function Article({ title, date = "January 1, 1970",preview}) {
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+  const coffeeCups = Math.ceil(minutes / 5);
+  const bentoBoxes = Math.ceil(minutes / 10);
+
+  const renderCoffeeCups = () => {
+    return "☕️".repeat(coffeeCups);
+  };
+
+  const renderBentoBoxes = () => {
+    return "🍱".repeat(bentoBoxes);
+  };
+
   return (
     <article>
       <h3>{title}</h3>
       <small>{date}</small>
       <p>{preview}</p>
+      <span>
+        {minutes < 30 ? renderCoffeeCups() : renderBentoBoxes()} {minutes} min read
+      </span>
     </article>
   );
 }
